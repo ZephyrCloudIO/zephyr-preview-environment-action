8e17cc01f167f2bf50b7a27266ed5855b8f3c3d8
leooliveirax <xleonardo.x.oliveirax@gmail.com>
2025-07-02 18:54:10 -0300

    refactor: enhance pull request event handling in GitHub Actions by adding support for closed events and improving structure

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,35 @@
 import * as core from "@actions/core";
+import * as github from "@actions/github";
 
+import { handlePullRequestClosed } from "./handlers/pull-request-closed.handler";
 import { handlePullRequestOpened } from "./handlers/pull-request-opened.handler";
+import { handlePullRequestUpdated } from "./handlers/pull-request-updated.handler";
+import { isPullRequestEvent } from "./services/github/is-pull-request-event.service";
 
 (async () => {
   try {
-    // if (!isPullRequestEvent()) {
-    //   core.warning(
-    //     "This action can only be triggered on pull requests and pull request data must be available. Current event: " +
-    //       github.context.eventName,
-    //   );
+    if (!isPullRequestEvent()) {
+      core.warning(
+        "This action can only be triggered on pull requests and pull request data must be available. Current event: " +
+          github.context.eventName,
+      );
 
-    //   return;
-    // }
+      return;
+    }
 
-    await handlePullRequestOpened();
-
-    // const eventAction = github.context.payload.action;
-    // switch (eventAction) {
-    //   case "opened":
-    //     await handlePullRequestOpened();
-    //     break;
-    //   case "synchronize":
-    //   case "reopened":
-    //     await handlePullRequestUpdated();
-    //     break;
-    //   case "closed":
-    //     await handlePullRequestClosed();
-    //     break;
-    // }
+    const eventAction = github.context.payload.action;
+    switch (eventAction) {
+      case "opened":
+        await handlePullRequestOpened();
+        break;
+      case "synchronize":
+      case "reopened":
+        await handlePullRequestUpdated();
+        break;
+      case "closed":
+        await handlePullRequestClosed();
+        break;
+    }
   } catch (error) {
     core.setFailed(
       `Action failed: ${error instanceof Error ? error.message : JSON.stringify(error)}`,
