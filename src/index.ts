d0a5f835210768f499b8ece8ab28abbceaac66cd
leooliveirax <xleonardo.x.oliveirax@gmail.com>
2025-11-07 17:32:37 -0300

    feat: remove github validations

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,7 @@ import { context } from "@actions/github";
 import { handlePullRequestClosed } from "./handlers/pull-request-closed.handler";
 import { handlePullRequestOpened } from "./handlers/pull-request-opened.handler";
 import { handlePullRequestUpdated } from "./handlers/pull-request-updated.handler";
-import { validateGitHubToken } from "./services/github/validation/validate-github-token.service";
 import { validatePullRequestEvent } from "./services/github/validation/validate-pull-request-event.service";
-import { validateWorkflowPermissions } from "./services/github/validation/validate-workflow-permissions.service";
 
 (async () => {
   try {
@@ -18,9 +16,6 @@ import { validateWorkflowPermissions } from "./services/github/validation/valida
 
       return;
     }
-
-    await validateGitHubToken();
-    await validateWorkflowPermissions();
 
     const eventAction = context.payload.action;
     switch (eventAction) {
